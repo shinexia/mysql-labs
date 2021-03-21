0e9ba9e3e6a89bb9d70f4270bbe59705dcd905ff
shine.xgh <shine.xgh@gmail.com>
2021-03-21 11:09:54 +0800

    remove boost

--- build.sh
+++ build.sh
@@ -6,24 +6,20 @@ ROOT_DIR=$(pwd)
 
 MYSQL_SERVER_SRC_DIR=${ROOT_DIR}/mysql-server
 MYSQL_SERVER_BLD_DIR=${ROOT_DIR}/build
-MYSQL_SERVER_DST_DIR=${ROOT_DIR}/dist/mysql
+MYSQL_SERVER_DST_DIR=${ROOT_DIR}/dist/mysql-server
 
-BOOST_SRC_DIR=${ROOT_DIR}/boost
-BOOST_DST_DIR=${ROOT_DIR}/dist/boost
-
-function build_boost() {
-    cd ${BOOST_SRC_DIR}
-    ./bootstrap.sh --prefix=${BOOST_DST_DIR}
-    ./b2 install
-}
+BOOST_DIR=/data/opt/boost
 
 function build_mysql() {
     mkdir -p ${MYSQL_SERVER_BLD_DIR} && cd ${MYSQL_SERVER_BLD_DIR}
-    cmake ${MYSQL_SERVER_SRC_DIR} -DWITH_BOOST=${BOOST_DST_DIR}/include -DCMAKE_INSTALL_PREFIX=${MYSQL_SERVER_DST_DIR}
+    cmake ${MYSQL_SERVER_SRC_DIR} \
+        -DWITH_BOOST=${BOOST_DIR} \
+        -DDOWNLOAD_BOOST=1 \
+        -DCMAKE_INSTALL_PREFIX=${MYSQL_SERVER_DST_DIR}
     make -j$(nproc)
+    mkdir -p ${MYSQL_SERVER_DST_DIR}
     make install
 }
-
 
 function clean_all() {
     rm -frv build dist
@@ -31,16 +27,7 @@ function clean_all() {
 
 case $1 in
 
-build_boost)
-    build_boost
-    ;;
-
-build_mysql)
-    build_mysql
-    ;;
-
 build)
-    build_boost
     build_mysql
     ;;
 
